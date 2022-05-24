55bec0596cd61adb40a291c09f37fa93136a9173
1753024 <an.tranquoctruong@gmail.com>
2022-05-24 11:24:03 +0700

    add parameter

--- README.md
+++ README.md
@@ -1,16 +1,35 @@
 ## Automatic Acunetix
+```
+usage: app.py [-h] [--threads THREADS] [--speed SPEED] [--host HOST] URLs_File
 
-### 1.Create API token
+Make the automatic task more automatic.
 
-### 2.Create .env
+positional arguments:
+  URLs_File          List of urls
+
+optional arguments:
+  -h, --help         show this help message and exit
+  --threads THREADS  Number of tasks that run simultaneously.
+  --speed SPEED      The speed of the scan.
+  --host HOST        The host of the acunetix.
+```
+
+### Install
+```bash
+pip3 install -r requirements.txt 
+```
+### Setup
+#### 1.Create API token
+
+#### 2.Create .env
 
 ```
 API="<Token>"
 ```
 
-### 3.Create file list of urls need scan name "urls.txt"
+#### 3.Create file list of urls need scan name "urls.txt"
 
-### 4.Run file
+#### 4.Run file
 
 ```
 python3 app.py
--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from pydoc import resolve
+import string
 from urllib import response
 import requests
 import os
 from dotenv import load_dotenv
 from yaml import scan
 import time
+import argparse
+import sys
+
 
 from urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
@@ -17,6 +21,8 @@ header = {
     "X-Auth": API_KEY
 }
 session = requests.Session()
+#######################################################################
+# API
 
 
 def getScan(hostname, id):
@@ -54,6 +60,42 @@ def scanTarget(hostname, target_id):
     return response
 
 
+def configTarget(hostname, target_id, scan_speed):
+    url = "{}/api/v1/targets/{}/configuration".format(hostname, target_id)
+    body = {
+        "description": "Target configuration default values",
+        "limit_crawler_scope": True,
+        "login": {
+            "kind": "none"
+        },
+        "sensor": False,
+        "ssh_credentials": {
+            "kind": "none"
+        },
+        "proxy": {
+            "enabled": False
+        },
+        "authentication": {
+            "enabled": False
+        },
+        "client_certificate_password": "",
+        "scan_speed": scan_speed,
+        "case_sensitive": "auto",
+        "technologies": [],
+        "custom_headers": [],
+        "custom_cookies": [],
+        "excluded_paths": [],
+        "user_agent": "",
+        "debug": False
+    }
+    response = session.patch(
+        url, json=body, headers=header, verify=False).status_code
+    return response
+
+#######################################################################
+# User
+
+
 def writeToFile(filename, strings):
     file = open(filename, 'w+')
     for string in strings:
@@ -67,22 +109,24 @@ def readFile(filename):
     return [string.strip('\n') for string in strings]
 
 
-def createTargetAndScan(target_url):
+def slowdownScan(target_id, mode):
+    response = configTarget(HOST, target_id,mode)
+    return response
+
+
+def createTargetAndScan(target_url, mode="fast"):
     target_id = createTarget(HOST, target_url)["target_id"]
+    if (mode != "fast"):
+        slowdownScan(target_id, mode)
     scan_id = scanTarget(HOST, target_id)["scan_id"]
     return scan_id
 
 
 def isScanComplete(scan_id):
     result = getScan(HOST, scan_id)["current_session"]["status"]
-    if (result == "completed"):
-        return True
-    return False
-
-
-TARGET_LIST = "urls.txt"
-MAX_THREAD = 3
-HOST = "https://localhost:13443"
+    if (result == "processing"):
+        return False
+    return True
 
 
 def main():
@@ -103,20 +147,52 @@ def main():
         max_thread -= running_threads_number
     if os.path.exists(TARGET_LIST):
         targets = readFile(TARGET_LIST)
-    else: 
+    else:
         print("Missing urls file!")
         exit()
 
     for target in targets[:max_thread]:
-        thread = createTargetAndScan(target)
+        thread = createTargetAndScan(target,SPEED)
         running_threads.append(thread)
     writeToFile(stack_file, running_threads)
     writeToFile(TARGET_LIST, targets[max_thread:])
+def getArgs():
+    parser = argparse.ArgumentParser(description='Make the automatic task more automatic.')
+    parser.add_argument('urls_file',metavar="URLs_File", type=argparse.FileType('r'),help='List of urls')
+    parser.add_argument('--threads', type=int, default=3, help='Number of tasks that run simultaneously.')
+    parser.add_argument("--speed", default="fast",help='The speed of the scan.')
+    parser.add_argument("--host", default="https://localhost:13443",help='The host of the acunetix.')
+
+
+    args = parser.parse_args()
+    return args
+def setGlobal(args):
+    global TARGET_LIST
+    global MAX_THREAD
+    global HOST
+    global SPEED
+    threads = args.threads
+    target_file=args.urls_file.name
+    host=args.host
+    speed = args.speed
+    if(speed != "fast" and speed != "moderate"and speed != "slow"and speed != "sequential"):
+        print("Wrong speed!")
+        exit()
+    
+    TARGET_LIST = target_file
+    MAX_THREAD = threads
+    HOST = host
+    SPEED = speed
+
 
 
 if __name__ == "__main__":
-    # print(getScan(HOST,"1a990c5b-44a0-4778-91da-30209230f756"))
-    print("running...")
+   
+    args = getArgs()
+    setGlobal(args)
+    print("""Script running...
+    Ctrl + C to stop.
+    """)
     while True:
         try:
             main()
